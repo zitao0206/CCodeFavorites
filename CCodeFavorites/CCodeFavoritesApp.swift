4eb78563216a96cbe36800ca455704aebce0b3be
lizitao <634376133@qq.com>
2023-09-11 14:28:45 +0200

    initialize the project

---
+++ CCodeFavorites/CCodeFavoritesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCodeFavoritesApp.swift
+//  CCodeFavorites
+//
+//  Created by lizitao on 2023/9/11.
+//
+
+import SwiftUI
+
+@main
+struct CCodeFavoritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
